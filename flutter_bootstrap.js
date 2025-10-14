8423b0782db8989de6b01c226df6ec313052a4ea
Ken-App <kenapplications@gmail.com>
2025-10-14 20:12:00 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1374816981"
+    serviceWorkerVersion: "3373619454"
   }
 });
